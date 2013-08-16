eadb3db8c378c736022e9f23cd7c430161a0ab56
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-16 10:25:56 -0700

    Add comments to Designs Controller.

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,46 +1,55 @@
 class DesignsController < ApplicationController
   respond_to :json
+  # Expect only JSON formants
 
+  # Set the #design variable pased on params[:id] for all
+  # actions, except for :index, :create
+  # so do it in the before_action callback
+  # because it is more DRY
   before_action :get_design, except: [ :index, :create ]
 
   # GET
   def index
+    # Get array of all designs
     @designs = Design.all.entries
-
     render :json => @designs
   end
 
   def show
-    if @design
-      render :json => @design
+    head :not_found unless @design
+    # If design with passed ID was not found,
+    # show 404 Not Found HTTP Status in get_design
     end
   end
 
   # POST
   def create
     @design = Design.new(design_params)
-
+    # Try to create a new design with the pased params
     if @design.save
       render :json => @design, status: :created
+      # If successfully created, return as JSON with 201 Status
     else
-      render :json => @design.errors, status: :unprocessable_entity
+      render :errors, status :unprocessable_entity
+      # If not, render errors template with 422 Status
     end
   end
 
   # PUT
   def update
-    if @design
-      if @design.update_attributes(design_params)
-        head :no_content
-      else
-        render :json => @design.errors, status: :unprocessable_entity
+    if @design # If param is found,
+      if @design.update_attributes(design_params) # Try to update it
+        head :no_content # If successful, return Status 204
+      else # Unsuccessful return, render unprocessable entity
+        render :errors, status: :unprocessable_entity
       end
     end
   end
+  # Otherwise, fall through to no content in get_design below
 
   # DELETE
   def destroy
-    if @design
+    if @design # If found, destroy it and return 204 no content.
       @design.destroy
       head :no_content
     end
@@ -48,7 +57,7 @@ class DesignsController < ApplicationController
 
   private
 
-  def get_design
+  def get_design # Set 404 not found
     head :not_found unless @design = Design.find(params[:id])
   end
 
